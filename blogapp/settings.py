a1b7c68a6a6eae24c953d19758630a0cb42c99a5
Sanjay Bhandari <93391951+dashing-dev@users.noreply.github.com>
2022-06-22 07:36:47 +0545

    Update settings.py

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -23,7 +23,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-oik()ln_qadp9&7%mj*884%!06ml*rd*dd8lwck375gey_9&9^'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
